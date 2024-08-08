8b104046572f5f05768afcc367889a0ce4da877c
toshal <toshal.t1@gmail.com>
2024-08-08 11:19:38 +0530

    fix: fix experience

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -10,9 +10,9 @@ const Education = () => {
             </div>
             
             <div className="mt-20 md:mt-10 grid grid-cols-3 gap-4 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1">
-                <ExperienceCard icon="/images/avizva.png" date="janurary 2021 - present" heading="Software Engineer" description="Avizva,(Full time)" place="Gurgoan, india" />
-                <ExperienceCard icon="/images/shinedezign.png" date="janurary 2020 - janurary 2021" heading="Software Engineer" description="ShineDezign,(Full time)" place="Mohali, india" />
-                <ExperienceCard icon="/images/busibud.jpeg" date="october 2019 - janurary 2020" heading="Junior Frontend Engineer" description="Busibud,(Full time)" place="Mohali, india" />
+                <ExperienceCard icon="/images/avizva.png" date="Janurary 2021 - present" heading="Frontend Developer" description="Avizva,(Full time)" place="Gurgoan, india" />
+                <ExperienceCard icon="/images/shinedezign.png" date="February 2020 - Janurary 2021" heading="Software Development Engineer" description="ShineDezign,(Full time)" place="Mohali, india" />
+                <ExperienceCard icon="/images/busibud.jpeg" date="October 2019 - janurary 2021" heading="Junior Software Engineer" description="Busibud,(Full time)" place="Mohali, india" />
             </div>
         </div>
     )
